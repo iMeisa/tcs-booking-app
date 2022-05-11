102afb760a322198f20bb939a88cefc5a90b6a12
iMeisa <62470487+iMeisa@users.noreply.github.com>
2022-05-11 13:38:01 +0300

    23. Handler functions

--- main.go
+++ main.go
@@ -5,22 +5,25 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
-func AddValues(x, y int) int {
+func addValues(x, y int) int {
 	return x + y
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/about", About)
 
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
